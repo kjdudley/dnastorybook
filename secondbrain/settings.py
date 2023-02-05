87e16be697fd4c6b62bcbbd82621f588a6456e3a
Kevin Dudley <kevin.dudley1@gmail.com>
2023-02-05 18:58:07 +1000

    New local settings.

--- secondbrain/settings.py
+++ secondbrain/settings.py
@@ -136,3 +136,9 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
+    
